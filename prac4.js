439865eb2bc6f804bc4b8e1c0d06eae805b19135
zhavy001 <84431517+zhavy001@users.noreply.github.com>
2021-07-02 01:34:29 +0930

    Add files via upload

---
+++ prac4.js
@@ -0,0 +1,34 @@
+document.getElementById("img1link").onclick = function() 
+{
+    document.getElementById("img1link").innerText = "Pardalote";
+    document.getElementById("img1").src = "http://www.outgrabe.net/bird00.jpg";
+    document.getElementById("img1txt").innerHTML = "Pardalote by fir0002 (CC-by-NC)";
+}
+
+document.getElementById("img2link").onclick = function() 
+{
+    document.getElementById("img2link").innerText = "Purple Swamp Hen";
+    document.getElementById("img2").src = "http://www.outgrabe.net/bird01.jpg";
+    document.getElementById("img2txt").innerHTML = "Purple swamp hen by Toby Hudson (CC-by-SA)";
+}
+
+document.getElementById("img3link").onclick = function() 
+{
+    document.getElementById("img3link").innerText = "White-headed Stilt";
+    document.getElementById("img3").src = "http://www.outgrabe.net/bird02.jpg";
+    document.getElementById("img3txt").innerHTML = "White-headed Stilt by JJ Harrison (CC-by-SA)";
+}
+
+document.getElementById("img4link").onclick = function() 
+{
+    document.getElementById("img4link").innerText = "Inland Thornbill";
+    document.getElementById("img4").src = "http://www.outgrabe.net/bird03.jpg";
+    document.getElementById("img4txt").innerHTML = "Inland Thornbill by Peter Jacobs (CC-by-SA)";
+}
+
+document.getElementById("img5link").onclick = function() 
+{
+    document.getElementById("img5link").innerText = "Rose Robin";
+    document.getElementById("img5").src = "http://www.outgrabe.net/bird04.jpg";
+    document.getElementById("img5txt").innerHTML = "Rose Robin by JJ Harrison (CC-by-SA)";
+}
